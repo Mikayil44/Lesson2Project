d4793a90585a30d6e29eb8e75cd5aad8668f6a04
Mikayil44 <147695892+Mikayil44@users.noreply.github.com>
2025-09-26 18:44:45 +0400

    Add files via upload

---
+++ Information/MPL3/Form1.cs
@@ -0,0 +1,34 @@
+namespace MPL3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string gender ;
+            if (radioButton1.Checked) gender = radioButton1.Text;
+            else if (radioButton2.Checked) gender = radioButton2.Text;
+            else if (radioButton3.Checked) gender = radioButton3.Text;
+            else gender = "N/A";
+                string programmingLanguages = "";
+            if (checkBox1.Checked) programmingLanguages += checkBox1.Text + " ";
+            else if (checkBox2.Checked) programmingLanguages +=  checkBox2.Text+ " ";
+            else if (checkBox3.Checked) programmingLanguages +=   checkBox3.Text + " ";
+            else programmingLanguages = "N/A";
+            string country;
+            if (comboBox1.SelectedItem != null) country = comboBox1.SelectedItem.ToString();
+            else country = "N/A";
+            string city;
+            if (listBox1.SelectedItem != null) city = listBox1.SelectedItem.ToString();
+            else city = "N/A";
+            MessageBox.Show("Gender: "+gender+
+                            "\nCountry: "+country+
+                            "\nCity: "+city+
+                            "\nProgramming Languages: "+programmingLanguages);
+        }
+    }
+}
